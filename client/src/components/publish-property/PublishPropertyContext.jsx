65c2f392393f175eaeb00c81cd1b5a8c223c25f2
Gonzalo Blasco <gonzagabino@gmail.com>
2024-11-17 19:35:29 +0000

    Refactor PublishPropertyContext to manage form state and validation; update PublishProperty page to utilize context provider

--- client/src/components/publish-property/PublishPropertyContext.jsx
+++ client/src/components/publish-property/PublishPropertyContext.jsx
@@ -1,5 +1,80 @@
-import { createContext } from "react"
+import React, { createContext, useContext, useState } from "react"
 
+// Crear el Contexto
 const PublishPropertyContext = createContext(undefined)
+
+// Crear el Provider para envolver tus componentes
+export const PublishPropertyProvider = ({ children }) => {
+  const [formData, setFormData] = useState([
+    {
+      building_id: 0,
+    },
+  ])
+
+  const [errors, setErrors] = useState([
+    {
+      building_id: { hasError: false, message: "" },
+    },
+  ])
+
+  const validateStep1 = (setErrors, formData) => {
+    const { building_id } = formData
+    let isValid = true
+
+    if (!building_id || building_id.length < 10) {
+      setErrors((prevErrors) => {
+        const updatedErrors = [...prevErrors]
+        updatedErrors[0] = {
+          ...updatedErrors[0],
+          building_id: {
+            hasError: true,
+            message: "El número de edificio debe ser de 10 dígitos",
+          },
+        }
+        return updatedErrors
+      })
+      isValid = false
+    }
+
+    console.log(isValid)
+    console.log(formData)
+    return isValid
+  }
+
+  const nextStepFunction = [
+    () => validateStep1(setErrors, formData[0]),
+    // Aquí van los siguientes pasos
+  ]
+
+  const handleOnChange = (event, index) => {
+    const { name, value, type, checked } = event.target
+
+    const newValue = type === "checkbox" ? checked : value
+
+    setFormData((prevData) => {
+      const updatedData = [...prevData]
+      updatedData[index] = {
+        ...updatedData[index],
+        [name]: newValue,
+      }
+      return updatedData
+    })
+  }
+
+  return (
+    <PublishPropertyContext.Provider
+      value={{
+        formData,
+        setFormData,
+        errors,
+        setErrors,
+        nextStepFunction,
+        handleOnChange,
+      }}
+    >
+      {children}
+    </PublishPropertyContext.Provider>
+  )
+}
 
 export default PublishPropertyContext
--- client/src/pages/PublishProperty.jsx
+++ client/src/pages/PublishProperty.jsx
@@ -1,12 +1,11 @@
-import React from "react"
-import { useState } from "react"
+import React, { useState } from "react"
 import { Container } from "@mui/material"
 import HorizontalLinearStepper from "../components/HorizontalLinearStepper"
 import SelectBuilding from "../components/publish-property/SelectBuilding"
 // import Multimedia from "../components/publish-property/Multimedia"
 // import Characteristics from "../components/publish-property/Characteristics"
 // import ReviewProperty from "../components/publish-property/ReviewProperty"
-import PublishPropertyContext from "../components/publish-property/PublishPropertyContext"
+import { PublishPropertyProvider } from "../components/publish-property/PublishPropertyContext"
 
 function PublishProperty() {
   const steps = [
@@ -15,66 +14,6 @@ function PublishProperty() {
     // "Características",
     // "Revisión",
   ]
-
-  const validateStep1 = (setErrors, formData) => {
-    const { building_id } = formData
-    let isValid = true
-
-    if (!building_id || building_id.length < 10) {
-      setErrors((prevErrors) => {
-        const updatedErrors = [...prevErrors]
-        updatedErrors[0] = {
-          ...updatedErrors[0],
-          building_id: {
-            hasError: true,
-            message: "El número de edificio debe ser de 10 dígitos",
-          },
-        }
-        return updatedErrors
-      })
-      isValid = false
-    }
-
-    console.log(isValid)
-    console.log(formData)
-    return isValid
-  }
-
-  const nextStepFunction = [
-    () => validateStep1(setErrors, formData[0]),
-    // () => console.log("Estoy en la etapa 2"),
-    // () => console.log("Estoy en la etapa 3"),
-    // () => console.log("Estoy en la etapa 4"),
-  ]
-
-  const [formData, setFormData] = useState([
-    // Índice 0: Datos de la primera "etapa"
-    {
-      building_id: 0,
-    },
-  ])
-
-  const [errors, setErrors] = useState([
-    // Índice 0: Errores para la primera "etapa"
-    {
-      building_id: { hasError: false, message: "" },
-    },
-  ])
-
-  const handleOnChange = (event, index) => {
-    const { name, value, type, checked } = event.target
-
-    const newValue = type === "checkbox" ? checked : value
-
-    setFormData((prevData) => {
-      const updatedData = [...prevData]
-      updatedData[index] = {
-        ...updatedData[index],
-        [name]: newValue,
-      }
-      return updatedData
-    })
-  }
 
   const comps = [
     <SelectBuilding key={0} />,
@@ -93,18 +32,9 @@ function PublishProperty() {
         width: "90%",
       }}
     >
-      <PublishPropertyContext.Provider
-        value={{
-          formData,
-          setFormData,
-          errors,
-          setErrors,
-          nextStepFunction,
-          handleOnChange,
-        }}
-      >
+      <PublishPropertyProvider>
         <HorizontalLinearStepper componets={comps} steps={steps} />
-      </PublishPropertyContext.Provider>
+      </PublishPropertyProvider>
     </Container>
   )
 }
