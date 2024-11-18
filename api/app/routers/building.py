ff2e70423b2026e91e7809a4af88ee9195d57fe8
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2024-11-18 10:26:52 -0300

    add search endpoint for buildings; include response models and status codes

--- api/app/routers/building.py
+++ api/app/routers/building.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query, status
+from typing import Annotated
 from ..database import db_dependency
 from ..models import Building
 from ..schemas.buildings import BuildingsResponse, BuildingResponse
@@ -8,8 +9,8 @@ router = APIRouter(
     tags=["buildings"],
 )
 
-@router.get("/")
-def read_buildings(db : db_dependency):
+@router.get("/", response_model=BuildingsResponse, status_code=status.HTTP_200_OK)
+def read_buildings(db: db_dependency):
     buildings = db.query(Building).all()
 
     if buildings is None:
@@ -17,9 +18,18 @@ def read_buildings(db : db_dependency):
 
     return buildings
 
-@router.get("/{building_id}")
+@router.get("/{building_id}", response_model=BuildingResponse, status_code=status.HTTP_200_OK)
 def read_building(building_id: int, db : db_dependency):
     building = db.query(Building).filter(Building.id == building_id).first()
+
+    if building is None:
+        raise HTTPException(status_code=404, detail="Building not found")
+    
+    return parse_building_response(building)
+
+@router.get("/search/", response_model=BuildingResponse, status_code=status.HTTP_200_OK)
+def search_building(db: db_dependency, address : Annotated[str, Query()]):
+    building = db.query(Building).filter(Building.address == address).first()
 
     if building is None:
         raise HTTPException(status_code=404, detail="Building not found")
