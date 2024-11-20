c9cfb65be2fae255381658e89fc3013139df4a99
Mateo Romero <materomero04@gmail.com>
2024-11-20 12:10:15 -0300

    Uncomment BuildingCard rendering in Search component

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -2,15 +2,16 @@ import React, { Fragment } from "react"
 import SearchBar from "../components/SearchBar"
 import { useContext } from "react"
 import SearchContext from "../components/SearchContext"
+import BuildingCard from "../components/cards/BuildingCard"
 
 function Search() {
   const { properties } = useContext(SearchContext)
   return (
     <Fragment>
       <SearchBar />
-      {/* {properties.buildings.map((building) => (
+      {properties.buildings.map((building) => (
         <BuildingCard key={building.id} building={building} />
-      ))} */}
+      ))}
     </Fragment>
   )
 }
