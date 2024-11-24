dcf8ba96fcab8dc1a0dd92fd50955d5f229cb4d0
Gonzalo Blasco <gonzagabino@gmail.com>
2024-11-24 16:40:03 -0300

    Add submission state management to HorizontalLinearStepper component

--- client/src/components/HorizontalLinearStepper.jsx
+++ client/src/components/HorizontalLinearStepper.jsx
@@ -47,6 +47,7 @@ export default function HorizontalLinearStepper({
   const { nextStepFunction } = useContext(
     property ? PublishPropertyContext : PublishBuildingContext,
   )
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
   const [activeStep, setActiveStep] = useState(0)
 
@@ -55,6 +56,11 @@ export default function HorizontalLinearStepper({
     if (isValid) {
       setActiveStep((prevActiveStep) => prevActiveStep + 1)
     }
+    setIsSubmitting(true)
+    setTimeout(() => {
+      console.log("Formulario enviado")
+      setIsSubmitting(false)
+    }, 2000)
   }
 
   const handleBack = () => {
@@ -113,7 +119,11 @@ export default function HorizontalLinearStepper({
               >
                 Volver
               </Button>
-              <Button onClick={handleNext} sx={{ ml: "auto" }}>
+              <Button
+                onClick={handleNext}
+                sx={{ ml: "auto" }}
+                disabled={isSubmitting}
+              >
                 {activeStep === steps.length - 1 ? "Publicar" : "Siguiente"}
               </Button>
             </Box>
