a5b36482d94a46dccb604b4a16db6021d80adf30
Gonzalo Blasco <gonzagabino@gmail.com>
2024-11-25 12:14:55 -0300

    Enhance FilterList component with improved dropdown styling and shadow effects

--- client/src/components/filters/FilterList.jsx
+++ client/src/components/filters/FilterList.jsx
@@ -34,6 +34,28 @@ function FilterList({ filters, setFilters }) {
           vertical: "top",
           horizontal: "right",
         }}
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: "8px",
+              "&::before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: "45px",
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          },
+        }}
       >
         <Container
           sx={{
