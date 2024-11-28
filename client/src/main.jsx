31a2bb2f72ca038a68e672470bf573febce87948
Mateo Romero <materomero04@gmail.com>
2024-11-27 22:39:12 -0300

    Added comment on main file to teach Gonzalo how to solve merge conflicts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx"
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./hooks/AuthContext"
 
+// Hola
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
